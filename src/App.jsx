05b24562ce46b74bfeb0d5b3e40ceb4022ba3d08
Yusif Najafli <ynajafli@outlook.com>
2026-03-03 16:54:50 -0700

    added page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import PostPage from "./pages/PostPage";
+import NotFound from "./pages/NotFound";
 
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route>
     <Route path="/" element={ <HomePage /> } />
     <Route path="/posts/:postId" element={ <PostPage /> } />
+    <Route path="*" element={ <NotFound /> }/>
   </Route>
 ));
 
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+
+
+const NotFound = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>404</h1>
+      <p>Page not found.</p>
+    </div>
+  );
+}
+
+export default NotFound;
